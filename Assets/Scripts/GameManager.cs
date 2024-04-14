a0ab048ef7c7a1feee41de6d4aebd15dee17c1e4
Persik956 <matthew.lopatin@yandex.ru>
2024-04-14 16:40:19 +0700

    no message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,28 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour{
     [SerializeField] private GameObject GaOb;
     [SerializeField] private GameObject GaOb2;
     [SerializeField] private Vector2 startPos;
     [SerializeField] private Vector2 direction;
     [SerializeField] private bool directionChosen;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start(){}
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.touchCount > 0)
-        {
+    void Update(){
+
+        if (Input.touchCount > 0){
             Touch touch = Input.GetTouch(0);
             GaOb.transform.position = touch.position;
-            switch (touch.phase)
-            {
+            switch (touch.phase){
                 case TouchPhase.Began:
                     startPos = touch.position;
                     directionChosen = false;
@@ -36,14 +30,13 @@ public class GameManager : MonoBehaviour
                 case TouchPhase.Ended:
                     directionChosen = true;
                     break;
-            
-        }
-        if (directionChosen)
-        {
-            if(GaOb.transform.position == GaOb2.transform.position)
-                {
+
+            }
+            if (directionChosen){
+                if (GaOb.transform.position == GaOb2.transform.position){
                     print("bimbimbambam");
                 }
+            }
         }
     }
 }
